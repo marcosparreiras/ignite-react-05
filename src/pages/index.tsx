3e2c01a9594491abbab607e7b54e1e76db98de71
Marcos Parreiras <marcosalp1998@gmail.com>
2024-09-08 17:51:11 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
-import { styled } from "../styles";
-
-const Button = styled("button", {
-  backgroundColor: "$green300",
-});
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import shirt01 from "../assets/1.png";
+import shirt02 from "../assets/2.png";
+// import shirt03 from "../assets/3.png";
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Button>Enviar</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt01} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,89</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt02} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,89</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
